0ecace988d71a4e7c2df591aec40cbe25057be1b
Haris Zafeiropoulos <haris@zorba.her.hcmr.gr>
2020-08-31 17:36:42 +0300

    fix code in writing output

--- sequences_of_life/eukaryota/clear_the_doubles.py
+++ sequences_of_life/eukaryota/clear_the_doubles.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3.5
 
-import sys
-
 input = open("MIDORI_UNIQ_GB239_CO1_RDP_no_taxids.fasta","r")
-#output = open("MIDORI_single_species_input_for_PHAT_algo.fasta", "w+")
+output = open("MIDORI_single_species_input_for_PHAT_algo.fasta", "w+")
 
-# STEP 1
-counter = 0
 uniq_taxonomies = []
 double_taxonomies = set()
 
@@ -16,94 +12,20 @@ for line in input:
 	if line[0] == ">" :
 
 		checking_var = 1
-
-		counter += 1
-		print(counter)
 		taxon = ''
-		line = line.split("\t")
-		taxonomy= line[1]
+		elements = line.split("\t")
+		taxonomy= elements[1]
 
 		if taxonomy not in uniq_taxonomies:
 			uniq_taxonomies.append(taxonomy)
+			output.write(line)
 		else:
 			checking_var = 0
 			double_taxonomies.add(taxonomy)
 
 	else:
 		if checking_var == 1:
-                        output.write(line)
-                    else:
-                        continue
+			output.write(line)
+		else:
+			continue
 
-
-
-
-
-
-print(len(uniq_taxonomies))
-sys.exit(0)
-
-# STEP 2
-
-checking_var = 0
-#for line in input: 
-#    if line[0] == ">":
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#with open(input, "r") as input:
-#	records = []
-#	current_record = []
-#	for lineno, line in enumerate(input):
-#
-#		if lineno == 0:
-#			current_record.append(line)
-#
-#		elif lineno % 2 == 0:
-#			records.append(current_record)
-#			current_record = []
-#			current_record.append(line)
-#		else:
-#
-#			current_record.append(line)
-#
-#print("the records found are: ")
-#print(len(records))
-#
-#
-#counter = 0 
-#for record in records:
-#	counter += 1 
-#	print("i am looking to the record No:" + str(counter))
-#	taxonomy = record[0].split(" ")
-#	taxon= taxonomy[1:]
-#	name = ''
-#	for term in taxon[:-1]:
-#		name += term + " "
-#
-#
-#	if name in double_taxonomies:
-#		print("I found a double")
-#		double_taxonomies.remove(name)
-#		continue
-#	else:
-#		for term in record:
-#			output.write(term)
-#
-#
-#
-#
