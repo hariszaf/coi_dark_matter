9a4a796335d857aeac8f7f2c668a332719fecffd
Haris Zafeiropoulos <haris@zorba.her.hcmr.gr>
2020-12-11 21:36:52 +0200

    job for the Phat algorithm

--- sequences_of_life/eukaryota/phat_job_eukaryotes.sh
+++ sequences_of_life/eukaryota/phat_job_eukaryotes.sh
@@ -11,18 +11,25 @@
 #SBATCH --requeue
 
 
-
-
 ## Running PhAT asking for 1000 consensus sequences (by default)
-#/home1/haris/Desktop/stsm/sequences_of_life/gappa/bin/gappa prepare phat --taxonomy-file second_atte
-mpt/taxonomies_of_the_final_seqs_with_species.txt \
-#--sequence-file second_attempt/final_alignment_for_eukaryotes_with_species.fasta --target-size 1000 
---threads 20 \
-#--consensus-method majorities --out-dir second_attempt/phat_final_alignment
+#/home1/haris/Desktop/stsm/sequences_of_life/gappa/bin/gappa prepare phat   \
+#--taxonomy-file taxonomies_for_phat_final.tsv \
+#--sequence-file full_midori_alignment_no_taxon_levels_extra_codes_removed_no_tabs.aln \
+#--target-size 1000 \
+#--threads 20 \
+#--consensus-method majorities \
+#--out-dir phat_output
 
 # Setting the order level as min-tax-tevel
-/home1/haris/Desktop/stsm/sequences_of_life/gappa/bin/gappa prepare phat --taxonomy-file second_attem
-pt/taxonomies_of_the_final_seqs_with_species.txt \
---sequence-file second_attempt/final_alignment_for_eukaryotes_with_species.fasta --target-size 1000 -
--min-tax-level 3 --threads 20 \
---consensus-method majorities --out-dir second_attempt/phat_final_alignment
+/home1/haris/Desktop/stsm/sequences_of_life/gappa/bin/gappa prepare phat \
+--taxonomy-file taxonomies_for_phat_final.tsv \
+--sequence-file full_midori_alignment_no_taxon_levels_extra_codes_removed_no_tabs.aln \
+--target-size 1000 \
+--min-tax-level 4 \
+--threads 20 \
+--consensus-method majorities \
+--out-dir phat_output
+
+
+
+
