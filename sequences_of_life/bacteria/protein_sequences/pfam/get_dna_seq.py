25a1b944e6f8957fa31d32a47815f167c88872dc
hariszaf <haris-zaf@hcmr.gr>
2021-07-24 19:28:14 +0300

    change path for efetch and add sleep

--- sequences_of_life/bacteria/protein_sequences/pfam/get_dna_seq.py
+++ sequences_of_life/bacteria/protein_sequences/pfam/get_dna_seq.py
@@ -3,14 +3,15 @@
 # script name: get_dna_seq.py
 # developed by: Haris Zafeiropoulos
 # date: 2021.07.24
-# framework: darn project 
+# framework: darn project
 # usage: ./get_dna_seq.py  > SEQ_DNA.fasta
 
 
-import sys
+import sys, time
 import subprocess
 
 coordinates_file   = open('final_coordinates.tsv', 'r')
+log_file           = open('downloads.log', 'w+')
 
 for entry in coordinates_file: 
 
@@ -18,5 +19,8 @@ for entry in coordinates_file:
    seq_coordinates = entry.split(":")[1]
    start           = seq_coordinates.split("..")[0]
    end             = seq_coordinates.split("..")[1]
+   log_file.write(entry)
+   subprocess.run(["/home1/haris/programs/edirect/efetch", "-db", "nuccore", "-id", genome_id, "-seq_start", start, "-seq_stop", end, "-format", "fasta" ])
 
-subprocess.run(["/home/haris/edirect/efetch", "-db", "nuccore", "-id", genome_id, "-seq_start", start, "-seq_stop", end, "-format", "fasta" ])
+   time.sleep(0.4)
+
