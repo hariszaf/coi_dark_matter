6e168e91f85783cc4c8e39ec8dc02126f95bcdd6
hariszaf <haris-zaf@hcmr.gr>
2021-07-26 22:18:45 +0300

    scripts to get sequences from gb directly

---
+++ sequences_of_life/gb_scripts/dna_bacteria/mafft_job_orientation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -N 2
+#SBATCH -B 2:10:1
+#SBATCH --partition=batch
+
+#SBATCH --ntasks-per-node=15
+#SBATCH --cpus-per-task=1
+#SBATCH --hint=compute_bound
+#SBATCH --job-name="orient"
+
+#SBATCH --output=bacteria_mafft_orient.output
+#SBATCH --mail-user=haris.zafr@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --requeue
+
+
+export MAFFT_N_THREADS_PER_PROCESS="1"
+export MAFFT_MPIRUN="/usr/bin/mpirun -n 30 -npernode 15 -bind-to none"
+
+/mnt/big/Tools/mafft-7.453-with-extensions/bin/mafft --mpi --thread 15 --globalpair --adjustdirectionaccurately bacteria_sequences_taxonomy.fasta > aligned_bacteria_sequences_true_orientation.fasta
+
