1145944d5698da406fea77f04635740b03054d40
hariszaf <haris-zaf@hcmr.gr>
2021-08-03 19:27:33 +0300

    modifs

--- build_tree_of_life/consensus_seqs/align_consensus_seqs.sh
+++ build_tree_of_life/consensus_seqs/align_consensus_seqs.sh
@@ -5,20 +5,24 @@
 #SBATCH --mail-type=ALL
 #SBATCH --requeue
 #SBATCH -p batch
-#SBATCH -N 2
+#SBATCH -N 3
 #SBATCH --ntasks-per-node=15
 #SBATCH --mail-user=haris.zafr@gmail.com
 
 
 
 export MAFFT_N_THREADS_PER_PROCESS="1"
-export MAFFT_MPIRUN="/usr/bin/mpirun.openmpi -n 30 -npernode 15 -bind-to none"
+export MAFFT_MPIRUN="/usr/bin/mpirun.openmpi -n 45 -npernode 15 -bind-to none"
 
 export MAFFT_N_THREADS_PER_PROCESS="1"
-export MAFFT_MPIRUN="/usr/bin/mpirun.openmpi -n 30 -npernode 15 -bind-to none"
+export MAFFT_MPIRUN="/usr/bin/mpirun.openmpi -n 45 -npernode 15 -bind-to none"
 
 
-mpirun.openmpi  /mnt/big/Tools/mafft-7.453-with-extensions/bin/mafft --mpi --globalpair --maxiterate 1000 non_period_all_seqs_multileline.fasta >  all_consensus_aligned.fasta
+mpirun.openmpi  /mnt/big/Tools/mafft-7.453-with-extensions/bin/mafft --mpi \
+   --globalpair \
+   --adjustdirectionaccurately \
+   --maxiterate 1000 \
+   non_period_all_seqs_multileline.fasta >  all_consensus_aligned_adjust_dir.aln
 
 
 # Run the mafft alignment tool
